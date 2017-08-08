07d759fc60fd9d4a59c3b01a4a04a9a6af8971d0
Chris Deacy <chrisdothtml@gmail.com>
2017-08-08 07:48:19 -0500

    building out UI; adding post fetch test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,17 @@
-console.log('Hey, world!')
+const doc = window.document
+const BODY = doc.body
+const ELEM = doc.getElementById('content')
+
+async function getPosts () {
+  const response = await fetch('/api/posts')
+  return response.json()
+}
+
+async function init () {
+  const posts = await getPosts()
+
+  BODY.classList.remove('loading')
+  ELEM.innerHTML = `There are ${posts.length} posts in the db`
+}
+
+init().catch(console.error)
